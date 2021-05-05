ad0e9fd5e854218b3b16b7f067ee3bbcdc7b3672
juneyj114 <acto2002@naver.com>
2021-05-05 15:09:03 +0900

    1.0

---
+++ chatting_app_03/android/app/src/main/kotlin/com/fluttercoder/chatting_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.fluttercoder.chatting_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ chatting_app_06/lib/screens/chat_screen.dart
@@ -0,0 +1,21 @@
+import 'package:chatting_app/components/chat_card.dart';
+import 'package:chatting_app/models/chat.dart';
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text("채팅"),
+      ),
+      body: ListView(
+        children: List.generate(
+          chats.length,
+          (index) => ChatCard(chat: chats[index]),
+        ),
+      ),
+    );
+  }
+}
